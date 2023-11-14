16c610f61ab106db13cecfb8c08f4d6a5f20bfbe
Andrew Jerez <jerezcastroa@roverkids.org>
2023-11-14 13:42:36 -0500

    lesson 1.1 & 1.2

---
+++ prototype 1/Assets/scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
