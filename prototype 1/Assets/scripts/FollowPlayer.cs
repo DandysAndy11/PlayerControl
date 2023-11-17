e5e82c531b06c55cfb7b45da55e5ad1e286a328b
Andrew Jerez <jerezcastroa@roverkids.org>
2023-11-17 13:40:26 -0500

    Auto stash before merge of "main" and "origin/main"

--- prototype 1/Assets/scripts/FollowPlayer.cs
+++ prototype 1/Assets/scripts/FollowPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Follows the player vehicle
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
